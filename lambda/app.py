cd85086e956b459b8b920fd1d959b14766bc9631
Zlash65 <zarrar65@gmail.com>
2025-06-30 01:45:04 +0530

    feat: invoke aws bedrock from lambda and store output in s3

--- lambda/app.py
+++ lambda/app.py
@@ -1,8 +1,84 @@
 import json
-import requests
+import boto3
+import botocore.config
+
+from datetime import datetime
+
+def generate_dockerfile(language: str) -> str:
+  formatted_prompt = f"""
+  <|begin_of_text|><|start_header_id|>user<|end_header_id|>
+  ONLY Generate an ideal Dockerfile for {language} with best practices. Do not provide any description
+  Include:
+  - Base image
+  - Installing dependencies
+  - Setting working directory
+  - Adding source code
+  - Running the application
+  <|eot_id|>
+  <|start_header_id|>assistant<|end_header_id|>
+  """
+
+  body = {
+    "prompt": formatted_prompt,
+    "max_gen_len": 512,
+    "temperature": 0.5,
+  }
+
+  try:
+    bedrock = boto3.client("bedrock-runtime", region_name="ap-south-1",
+                            config=botocore.config.Config(read_timeout=300, retries={"max_attempts": 3}))
+    response = bedrock.invoke_model(body=json.dumps(body), model_name="meta.llama3-8b-instruct-v1:0")
+
+    response_content = response.get("body").read().decode("utf-8")
+    response_data = json.loads(response_content)
+    print(response_data)
+
+    dockerfile = response_data["generation"]
+    return dockerfile
+
+  except Exception as e:
+    print("Error generating Dockerfile:", e)
+    return ""
+
+def save_dockerfile(s3_key: str, s3_bucket: str, dockerfile: str) -> str:
+  s3 = boto3.client("s3")
+
+  try:
+    s3.put_object(
+      Body=dockerfile,
+      Bucket=s3_bucket,
+      Key=s3_key,
+      ContentType="text/plain",
+      ACL="public-read"
+    )
+
+    print("Dockerfile saved to S3")
+    return f"https://{s3_bucket}.s3.amazonaws.com/{s3_key}"
+
+  except Exception as e:
+    print("Error saving Dockerfile to S3:", e)
+    return ""
 
 def handler(event, context):
-  return {
+  event = json.loads(event["body"])
+  language = event["language"]
+
+  dockerfile = generate_dockerfile(language)
+  current_time = datetime.now().strftime("%H-%M-%S")
+  s3_bucket = "aws-bedrock-example"
+
+  if dockerfile:
+    s3_key = f"dockerfiles/{language}-{current_time}.Dockerfile"
+    dockerfile_url = save_dockerfile(s3_key, s3_bucket, dockerfile)
+    return {
       "statusCode": 200,
-      "body": json.dumps({"message": "Hello from Lambda!"})
-  }
+      "body": json.dumps({
+        "message": "Dockerfile generated",
+        "url": dockerfile_url
+      })
+    }
+  else:
+    return {
+      "statusCode": 500,
+      "body": json.dumps({"message": "Failed to generate Dockerfile"})
+    }
