26490d219dabc1e752bfc519fa858b55a8395cf2
Zlash65 <zarrar65@gmail.com>
2025-06-30 00:19:47 +0530

    chore: add dummy lambda code and file structure

---
+++ build.sh
@@ -0,0 +1,6 @@
+rm -rf lambda_build lambda.zip
+mkdir -p lambda_build
+pip3 install -r lambda/requirements.txt -t lambda_build
+cp lambda/app.py lambda_build
+cd lambda_build && zip -r ../lambda.zip .
+cd ..
